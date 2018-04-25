26eff0791e17c693c05a0dbf9b306d1143fdc509
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-25 17:24:30 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,12 +4,12 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 def speak_to_grandma(phrase)
-  if !phrase.upcase
-     "HUH?! SPEAK UP, SONNY!"
+  if phrase.upcase
+     "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else
-    "NO, NOT SINCE 1938!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
 # If you shout, she can hear you (or at least she thinks so)
