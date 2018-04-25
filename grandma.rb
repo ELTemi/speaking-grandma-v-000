67632838f9a308ccb97cd64232189ca1e6390e2d
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-25 17:22:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 def speak_to_grandma(phrase)
-  if !phrase.uppercase
+  if !phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN"
