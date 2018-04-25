6e38571102cbcaca36b90142e6ad325bb5917082
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-25 17:25:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@
 # unless you shout it (type in all capitals).
 def speak_to_grandma(phrase)
   if phrase.upcase
-     "NO, NOT SINCE 1938!"
+     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else
